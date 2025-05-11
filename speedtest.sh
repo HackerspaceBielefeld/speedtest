25c290db2acc3fd978e766551fc26414a48e2cc8
Giga-Pudding <56741807+Giga-Pudding@users.noreply.github.com>
2025-05-11 16:54:56 +0200

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,42 +1,49 @@
 #!/bin/bash
-output_file="speed.csv"
-url="speedtest.wtnet.de"
+output_file="/root/speed.csv"
+iperf_path="/usr/bin/iperf3"
+server="speedtest.bitel.io"
+downloadport="5206"
+uploadport="5207"
+parallel_streams="4"
+ip_version="6"
+
 
 if [ ! -f $output_file ]; then
     echo "Time,Download,Upload" > "$output_file"
 fi
 
 timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-echo "Starting download speed test"
+echo "Starting download speed test" >&2
 while true; do
-    result=$(iperf3  -c $url -p 5200 -P 10 -4 -R 2>&1)
+    result=$($iperf_path -c $server -p $downloadport -P $parallel_streams -$ip_version -R 2>&1)
     if [[ "$result" == *"the server is busy"* ]]; then
-        echo "Server busy, retrying in 5 seconds..."
+        echo "Server busy, retrying in 5 seconds..." >&2
         sleep 5
     else
         download_speed=$(echo "$result" | grep "\[SUM\].*receiver" | awk '{print $6}')
         if [[ -n "$download_speed" ]]; then
-            echo "Download test complete."
+            echo "Download test complete." >&2
             break
         else
-            echo "Download test completed but couldn't parse speed, retrying in 5 seconds..."
+            echo "Download test completed but couldn't parse speed, retrying in 5 seconds..." >&2
             sleep 5
         fi
     fi
 done
-echo "Starting upload speed test"
+sleep 300
+echo "Starting upload speed test" >&2
 while true; do
-    result=$(iperf3  -c $url -p 5200 -P 10 -4 2>&1)
+    result=$($iperf_path -c $server -p $uploadport -P $parallel_streams -$ip_version 2>&1)
     if [[ "$result" == *"the server is busy"* ]]; then
-        echo "Server busy, retrying in 5 seconds..."
+        echo "Server busy, retrying in 5 seconds..." >&2
         sleep 5
     else
         upload_speed=$(echo "$result" | grep "\[SUM\].*sender" | awk '{print $6}')
         if [[ -n "$upload_speed" ]]; then
-            echo "Upload test complete."
+            echo "Upload test complete." >&2
             break
         else
-            echo "Upload test completed but couldn't parse speed, retrying in 5 seconds..."
+            echo "Upload test completed but couldn't parse speed, retrying in 5 seconds..." >&2
             sleep 5
         fi
     fi
